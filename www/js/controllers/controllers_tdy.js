1f0ea0b5f83917f9018449d847e4c486aa3e938a
SimonTDY <584530365@qq.com>
2017-04-27 13:05:22 +0800

    Signed-off-by: SimonTDY <584530365@qq.com>

--- www/js/controllers/controllers_tdy.js
+++ www/js/controllers/controllers_tdy.js
@@ -1067,8 +1067,8 @@ VitalSign.getVitalSigns({userId:'U201702071766',type:'心率'}).then(
 //健康详情--PXY
 .controller('HealthDetailCtrl', ['ionicDatePicker','$scope','$state','$ionicHistory','$ionicPopup','$stateParams','$ionicPopover','$ionicModal','$ionicScrollDelegate','$ionicLoading','$timeout','Dict','Health','Storage','Camera',function(ionicDatePicker,$scope, $state,$ionicHistory,$ionicPopup,$stateParams,$ionicPopover,$ionicModal,$ionicScrollDelegate,$ionicLoading,$timeout,Dict,Health,Storage,Camera) {
   $scope.barwidth="width:0%";
-  var patientId = Storage.get('getpatientId')
-  // var patientId = 'U201702071766'   //测试ID
+  // var patientId = Storage.get('getpatientId')
+  var patientId = 'U201702071766'   //测试ID
 
   // $scope.test = function(){
   //   console.log($scope.datepickerObject4);
