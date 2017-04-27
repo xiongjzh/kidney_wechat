c9105821f1ec89f54bb1c4dda029f3f4809aa31d
SimonTDY <584530365@qq.com>
2017-04-27 13:35:08 +0800

    Signed-off-by: SimonTDY <584530365@qq.com>

--- www/js/controllers/controllers_tdy.js
+++ www/js/controllers/controllers_tdy.js
@@ -1065,7 +1065,7 @@ VitalSign.getVitalSigns({userId:'U201702071766',type:'心率'}).then(
 
 
 //健康详情--PXY
-.controller('HealthDetailCtrl', ['ionicDatePicker','$scope','$state','$ionicHistory','$ionicPopup','$stateParams','$ionicPopover','$ionicModal','$ionicScrollDelegate','$ionicLoading','$timeout','Dict','Health','Storage','Camera',function(ionicDatePicker,$scope, $state,$ionicHistory,$ionicPopup,$stateParams,$ionicPopover,$ionicModal,$ionicScrollDelegate,$ionicLoading,$timeout,Dict,Health,Storage,Camera) {
+.controller('HealthDetailCtrl', ['ionicDatePicker','$scope','$state','$ionicHistory','$ionicPopup','$stateParams','$ionicPopover','$ionicModal','$ionicScrollDelegate','$ionicLoading','$timeout','Dict','Health','Storage','Camera','$location','wechat',function(ionicDatePicker,$scope, $state,$ionicHistory,$ionicPopup,$stateParams,$ionicPopover,$ionicModal,$ionicScrollDelegate,$ionicLoading,$timeout,Dict,Health,Storage,Camera,$location,wechat) {
   $scope.barwidth="width:0%";
   // var patientId = Storage.get('getpatientId')
   var patientId = 'U201702071766'   //测试ID
