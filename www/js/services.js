14af1a1d547cdc7a1d430e8a5e986fe0e7646c85
Jiazhen Xiong <xiongjzh@gmail.com>
2017-05-03 21:14:47 +0800

    会诊

--- www/js/services.js
+++ www/js/services.js
@@ -846,6 +846,7 @@ angular.module('kidney.services', ['ionic','ngResource'])
 
     var Communication =function(){
         return $resource(CONFIG.baseUrl + ':path/:route',{path:'communication'},{
+            conclusion:{method:'POST', params:{route: 'conclusion'}, timeout: 100000},
             getCommunication:{method:'GET', params:{route: 'getCommunication'}, timeout: 100000},
             getCounselReport:{method:'GET', params:{route: 'getCounselReport'}, timeout: 100000},
             getTeam:{method:'GET', params:{route: 'getTeam'}, timeout: 100000},
@@ -1276,7 +1277,18 @@ angular.module('kidney.services', ['ionic','ngResource'])
         });
         return deferred.promise;
     };
-    
+    self.conclusion = function(params){
+        var deferred = $q.defer();
+        Data.Communication.conclusion(
+            params,
+            function(data, headers){
+                deferred.resolve(data);
+            },
+            function(err){
+                deferred.reject(err);
+        });
+        return deferred.promise;
+    };
 
     return self;
 }])
